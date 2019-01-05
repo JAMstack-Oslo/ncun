72dcc9a19f7331783162f2e6fe749390b59a8767
Alexander Alemayhu <alexander@alemayhu.com>
2019-01-05 16:41:12 +0100

    prepare home page for use
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,5 @@
 <template>
   <Layout>
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
     <h1>Companies in 🇳🇴 using</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores</p>
   </Layout>
