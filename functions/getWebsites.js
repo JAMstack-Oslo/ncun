5b8c5f183a5e3c8f58a1d429ec92a6f1b97e74bd
Aman <aman.mender@tegningtil3d.no>
2019-01-05 17:54:06 +0100

    Output companies with a homepage

--- functions/getWebsites.js
+++ functions/getWebsites.js
@@ -1,7 +1,9 @@
 const request = require('request');
 const brregAPIurl = 'https://data.brreg.no/enhetsregisteret/api/enheter/?page=0&size=100';
 
-exports.handler = (evt,ctx,cb) => {
+exports.handler = (evt,ctx,callback) => {
+
+    /*
     checkIfUsingNetlify = () => {
         // Return only companies using netlify
     }
@@ -11,9 +13,9 @@ exports.handler = (evt,ctx,cb) => {
         return companies.filter(c => {
             return c.hasOwnProperty('hjemmeside')
         });
-    }
+    }*/
     // Static params - 100 companies
-    const find = new Promise(function(resolve,reject) {        
+    /*const find = new Promise(function(resolve,reject) {
         request.get(brregAPIurl, (e,r) => {
             if(e) {
                 console.error(e);
@@ -28,12 +30,31 @@ exports.handler = (evt,ctx,cb) => {
         console.log("Companies ",companies.length);
         cb(null, {
             statusCode: 200,
-            body: {'message':'Hello World!'}
+            body: JSON.stringify({'message':'Hello World!'}):
         })
     })
     find.catch(e => {
-        cb(e,{statusCode: 500, body: e});
+        console.error('Error called ',e);
+        cb(null,{statusCode: 500, body: e});
     })
 
-    find();
+    find();*/
+
+    
+    request.get(brregAPIurl, (e,r) => {
+        if(e) {
+            console.error(e);
+            //reject(e)
+            callback(e);
+        } else {
+            const parsedData = JSON.parse(r.body);
+            //resolve(parsedData._embedded.enheter)
+
+            callback(null, {
+                statusCode: 200,
+                body: JSON.stringify(parsedData._embedded.enheter.filter(c=>c.hasOwnProperty('hjemmeside')))
+            })
+        }
+    })
+
 }
