b178a4402d361e7ddffadbec00fe5d3966d51820
Alexander Alemayhu <alexander@alemayhu.com>
2019-01-05 17:19:15 +0100

    pages: sketch out the company structure
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,44 @@
 <template>
   <Layout>
     <h1>Companies in 🇳🇴 using Netlify</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores</p>
+    <hr>
+    <ul class="companies_list">
+      <li v-for="company in companies">
+        <h3>
+          <a :href="company.website">
+          <img v-if="company.favicon" :src="company.favicon">
+          {{ company.name }}
+          </a>
+          </h3>        
+      </li>
+    </ul>
+
   </Layout>
 </template>
 
+<style>
+.companies_list {
+      list-style-type: none;
+}
+</style>
+
 <script>
-export default {}
+const companies = [
+  {
+    name: 'Solutus',
+    website: 'https://solutus.no'
+  },
+  {
+    name: 'Google',
+    website: 'https://google.com',
+    favicon: 'https://google.com/favicon.ico'
+  }
+];
+export default {
+  data () {
+    return {
+      companies: companies
+    }
+  },
+}
 </script>
