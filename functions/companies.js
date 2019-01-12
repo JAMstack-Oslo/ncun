16112db38620ef53bce7d9e7b501905ba7f08757
Alexander Alemayhu <alexander@alemayhu.com>
2019-01-12 15:15:22 +0100

    Make the lambda function some data for us
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

---
+++ functions/companies.js
@@ -0,0 +1,28 @@
+const request = require("request");
+
+const exampleURL =
+  "https://data.brreg.no/enhetsregisteret/api/enheter/?page=0&size=100";
+
+exports.handler = (evt, ctx, callback) => {
+  // TODO: use isun to filter the list
+  const companiesWithUrl = companies => {
+    return companies
+      .filter(c => c.hasOwnProperty("hjemmeside"))
+      .map(c => {
+        return { name: c.navn, website: c.hjemmeside };
+      });
+  };
+
+  request.get(exampleURL, (e, r) => {
+    if (e) {
+      callback(null, { statusCode: 400, body: e.toString() });
+    } else {
+      const parsedData = JSON.parse(r.body);
+      const comps = companiesWithUrl(parsedData._embedded.enheter);
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(comps)
+      });
+    }
+  });
+};
